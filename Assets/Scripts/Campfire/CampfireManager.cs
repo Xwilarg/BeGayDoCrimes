9727c055bdd03e6cc224c41c83e508d31c76bb5e
Xwilarg <xwilarg@protonmail.com>
2023-10-18 23:42:38 +0200

    Load VN from campfire

---
+++ Assets/Scripts/Campfire/CampfireManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace YuriGameJam2023.Campfire
+{
+    public class CampfireManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            SceneManager.LoadScene("VN", LoadSceneMode.Additive);
+        }
+    }
+}
