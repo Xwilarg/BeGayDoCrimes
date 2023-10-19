67518b327b985b8d4cd1053754a2415ac4bae37c
Xwilarg <xwilarg@protonmail.com>
2023-10-19 22:45:31 +0200

    Fix broken textures

--- Assets/Scripts/Campfire/CampfireManager.cs
+++ Assets/Scripts/Campfire/CampfireManager.cs
@@ -1,13 +1,34 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using YuriGameJam2023.Player;
 
 namespace YuriGameJam2023.Campfire
 {
     public class CampfireManager : MonoBehaviour
     {
+        private Light _selected;
+
         private void Awake()
         {
             SceneManager.LoadScene("VN", LoadSceneMode.Additive);
+        }
+
+        private void Update()
+        {
+            if (_selected != null)
+            {
+                _selected.gameObject.SetActive(false);
+            }
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit) && hit.collider.CompareTag("Player"))
+            {
+                _selected = hit.collider.GetComponentInChildren<Light>();
+                _selected.gameObject.SetActive(true);
+            }
+            else
+            {
+                _selected = null;
+            }
         }
     }
 }
