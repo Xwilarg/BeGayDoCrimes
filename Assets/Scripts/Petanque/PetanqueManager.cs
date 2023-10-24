4acddab108f8948906777105421be84449bc22b9
Xwilarg <xwilarg@protonmail.com>
2023-10-24 18:12:58 +0200

    Start pétanque minigame

---
+++ Assets/Scripts/Petanque/PetanqueManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace YuriGameJam2023.Petanque
+{
+    public class PetanqueManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _prefab;
+
+        [SerializeField]
+        private Transform _spawnPoint;
+
+        [SerializeField]
+        private float _xMin;
+    }
+}
