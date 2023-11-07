a5bd69584f00b6e5e8aabe4b3a94872f0ad239b6
Xwilarg <xwilarg@protonmail.com>
2023-11-08 00:23:41 +0100

    Add track scrolling on scene 3

---
+++ Assets/Scripts/Map/TrackScroller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace YuriGameJam2023.Map
+{
+    public class TrackScroller : MonoBehaviour
+    {
+        [SerializeField]
+        private float _size;
+
+        [SerializeField]
+        private float _min;
+
+        [SerializeField]
+        private GameObject[] _tracks;
+
+        [SerializeField]
+        private float _speed;
+
+        private void Update()
+        {
+            foreach (var track in _tracks)
+            {
+                track.transform.Translate(-Time.deltaTime * _speed, 0f, 0f);
+                if (track.transform.position.z < _min)
+                {
+                    track.transform.Translate(-_min * 2f, 0f, 0f);
+                }
+            }
+        }
+    }
+}
