592e28fc3c42cb727496df6dca40b45824d37d60
Xwilarg <xwilarg@protonmail.com>
2023-11-07 02:11:42 +0100

    Link main menu to pétanque

---
+++ Assets/Scripts/Menu/MenuCampfire.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace YuriGameJam2023.Menu
+{
+    public class MenuCampfire : MonoBehaviour
+    {
+        [SerializeField]
+        private Light _light;
+        private Color _orColor;
+
+        private void Awake()
+        {
+            _orColor = _light.color;
+        }
+
+        private void OnMouseEnter()
+        {
+            _light.color = Color.red;
+        }
+
+        private void OnMouseExit()
+        {
+            _light.color = _orColor;
+        }
+
+        private void OnMouseUpAsButton()
+        {
+            SceneManager.LoadScene("Pétanque");
+        }
+    }
+}
