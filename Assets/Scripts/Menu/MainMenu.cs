20cfc0e378866b83175dac20e02dff9bf0142375
Xwilarg <xwilarg@protonmail.com>
2023-11-09 22:35:26 +0100

    Load props depending of game state

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,12 +1,32 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using YuriGameJam2023.Persistency;
 
 namespace YuriGameJam2023.Menu
 {
     public class MainMenu : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject _oldVersion, _newVersion;
+
+        private void Awake()
+        {
+            if (PersistencyManager.Instance.SaveData.CurrentLevel == 5)
+            {
+                _newVersion.gameObject.SetActive(true);
+            }
+            else
+            {
+                _oldVersion.gameObject.SetActive(true);
+            }
+        }
+
         public void Play()
         {
+            if (PersistencyManager.Instance.SaveData.CurrentLevel == 5)
+            {
+                PersistencyManager.Instance.DeleteSaveFolder();
+            }
             SceneManager.LoadScene("Main");
         }
     }
