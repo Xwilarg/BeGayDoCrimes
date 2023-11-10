95728384e76ea77ff7b961dd7ee860411ee0e693
Xwilarg <xwilarg@protonmail.com>
2023-11-10 20:17:07 +0100

    Fix level select scene having incorrect scene nb

--- Assets/Scripts/Menu/LevelSelect.cs
+++ Assets/Scripts/Menu/LevelSelect.cs
@@ -6,6 +6,11 @@ namespace YuriGameJam2023.Menu
 {
     public class LevelSelect : MonoBehaviour
     {
+        private void Awake()
+        {
+            SceneManager.LoadScene("DebugManager", LoadSceneMode.Additive);
+        }
+
         public void PlayLevel(int level)
         {
             PersistencyManager.Instance.SaveData.CurrentLevel = level;
