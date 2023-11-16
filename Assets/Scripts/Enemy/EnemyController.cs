43b7eb27130b5419648e420951ee426929d427f4
Indra <wuppie55@gmail.com>
2023-11-16 23:54:20 +0100

    Kill enemy when they cannot move

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -160,7 +160,11 @@ namespace YuriGameJam2023
             var path = new NavMeshPath();
             if (!_navigation.CalculatePath(target.transform.position, path))
             {
-                Debug.LogError("Cannot calculate path to target, this is bad!");
+                Debug.LogError("Cannot calculate path for " + name + " to target, this is bad!");
+
+                // You have been bad, now die
+                Die();
+                return;
             }
 
             _navigation.path = path;
