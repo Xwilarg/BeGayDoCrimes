b92b6ab0dfb9d0b8e119ca61f71931a33cced10b
Xwilarg <xwilarg@protonmail.com>
2023-11-17 01:58:08 +0100

    Shorten achievements hints

--- Assets/Scripts/Achievement/AchievementManager.cs
+++ Assets/Scripts/Achievement/AchievementManager.cs
@@ -56,9 +56,9 @@ namespace YuriGameJam2023.Achievement
             { AchievementID.Petanque, new() { Name = "Cracking the Egg", Description = "Play a full game of pétanque", Hint = "Get more of these and come back" } },
             { AchievementID.Effects4, new() { Name = "Feeling Under the Weather?", Description = "Have an enemy under 4 differents effects", Hint = "The hardest stains requires the strongest cleaning efforts" } },
             { AchievementID.Insult3, new() { Name = "I'm not a Rapper", Description = "Insult 3 enemies at once", Hint = "With bad enough words, you can easily ruin everyone day!" } },
-            { AchievementID.Cancel, new() { Name = "Innovative Strategy", Description = "Attempt to use both spiderweb and fire on a target", Hint = "Who through playing this game would require paying attention?!" } },
-            { AchievementID.KillGeneral, new() { Name = "Four Girls Army", Description = "Kill the general in the last level", Hint = "Kill the strongest enemy of the game then go directly to join without passing go" } },
-            { AchievementID.BurnHouse, new() { Name = "What are we Even Fighting for Anymore", Description = "Attempt to burn the girl's house", Hint = "The first level sure would go by faster if the plot point were to disappear" } },
+            { AchievementID.Cancel, new() { Name = "Innovative Strategy", Description = "Attempt to use both spiderweb and fire on a target", Hint = "Who through playing would require paying attention?!" } },
+            { AchievementID.KillGeneral, new() { Name = "Four Girls Army", Description = "Kill the general in the last level", Hint = "Flex on the game by killing its strongest enemy" } },
+            { AchievementID.BurnHouse, new() { Name = "What are we Even Fighting for Anymore", Description = "Attempt to burn the girl's house", Hint = "If only I could make the level 1 plot disappear" } },
 
             { AchievementID.Rel_MC, new() { Name = "Sticky Parental Issues", Description = "Reach max support for Makra and Claire", Hint = "Get two specifics characters to love each other enough" } },
             { AchievementID.Rel_WC, new() { Name = "Not Fucking Around", Description = "Reach max support for Willow and Claire", Hint = "Get two specifics characters to love each other enough"} },
