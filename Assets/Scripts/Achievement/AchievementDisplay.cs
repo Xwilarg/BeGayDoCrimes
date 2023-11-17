093a8fe768a31afa2bd8d65aa8b8c55a7d472fbe
Xwilarg <xwilarg@protonmail.com>
2023-11-17 01:29:27 +0100

    Make more free space in achievement panel

--- Assets/Scripts/Achievement/AchievementDisplay.cs
+++ Assets/Scripts/Achievement/AchievementDisplay.cs
@@ -17,6 +17,20 @@ namespace YuriGameJam2023.Achievement
         [SerializeField]
         private Button _fiftyPercents;
 
+        private bool _displayCompletion = true;
+
+        public void DisplayCompletion()
+        {
+            _displayCompletion = true;
+            Refresh();
+        }
+
+        public void DisplayOthers()
+        {
+            _displayCompletion = false;
+            Refresh();
+        }
+
         private void Awake()
         {
             Instance = this;
@@ -33,6 +47,9 @@ namespace YuriGameJam2023.Achievement
             for (int i = 0; i < _container.childCount; i++) Destroy(_container.GetChild(i).gameObject);
             foreach (var achievement in AchievementManager.Instance.Achievements)
             {
+                var isProg = achievement.Key == AchievementID.CompleteGame || achievement.Key.ToString().StartsWith("Rel_");
+                if ((_displayCompletion && !isProg) || (!_displayCompletion && isProg)) continue;
+
                 var a = Instantiate(_prefab, _container);
                 var unlocked = AchievementManager.Instance.IsUnlocked(achievement.Key);
                 if (!unlocked)
