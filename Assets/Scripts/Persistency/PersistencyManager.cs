46c562ef165bdc4be8ab8cfe7f62ac73358ee1e6
Indra <wuppie55@gmail.com>
2023-11-19 01:57:44 +0100

    Save encryption version 2

--- Assets/Scripts/Persistency/PersistencyManager.cs
+++ Assets/Scripts/Persistency/PersistencyManager.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 using UnityEngine;
 
@@ -7,17 +8,38 @@ namespace YuriGameJam2023.Persistency
 {
     public class PersistencyManager
     {
-        private readonly string _key = "ほんとやめて 嘘はやめて 期待してない方に進まないで 認めて ねえ認めて 「俺が馬鹿だった」って早く言え";
+        // Exactly 16 bytes
+        private const string _key = "嘘 はやめて";
 
-        private string Encrypt(string s)
+        private Aes CreateAes()
         {
-            StringBuilder str = new();
-            for (var i = 0; i < s.Length; i++)
-            {
-                str.Append((char)(s[i] ^ _key[i % _key.Length]));
-            }
-            return str.ToString();
+            var aes = Aes.Create();
+
+            aes.Key = Encoding.UTF8.GetBytes(_key);
+            aes.Mode = CipherMode.ECB;
+
+            return aes;
         }
+
+        private byte[] Encrypt(string s)
+        {
+            var aes = CreateAes();
+            var encryptor = aes.CreateEncryptor();
+
+            var data = Encoding.UTF8.GetBytes(s);
+            return encryptor.TransformFinalBlock(data, 0, data.Length);
+        }
+
+        private string Decrypt(byte[] d)
+        {
+            var aes = CreateAes();
+            var encryptor = aes.CreateDecryptor();
+
+            var data = encryptor.TransformFinalBlock(d, 0, d.Length);
+            return Encoding.UTF8.GetString(data);
+        }
+
+        private static string SaveFilePath => Path.Combine(Application.persistentDataPath, "save.sav");
 
         private static PersistencyManager _instance;
         public static PersistencyManager Instance
@@ -26,7 +48,7 @@ namespace YuriGameJam2023.Persistency
             {
                 if (_instance == null)
                 {
-                    Debug.Log($"[PER] Persistency Manager created, data will be saved at {Application.persistentDataPath}/save.bin");
+                    Debug.Log($"[PER] Persistency Manager created, data will be saved at {SaveFilePath}");
                     _instance = new();
                 }
                 return _instance;
@@ -40,9 +62,9 @@ namespace YuriGameJam2023.Persistency
             {
                 if (_saveData == null)
                 {
-                    if (File.Exists($"{Application.persistentDataPath}/save.bin"))
+                    if (File.Exists(SaveFilePath))
                     {
-                        _saveData = JsonConvert.DeserializeObject<SaveData>(Encrypt(File.ReadAllText($"{Application.persistentDataPath}/save.bin")));
+                        _saveData = JsonConvert.DeserializeObject<SaveData>(Decrypt(File.ReadAllBytes(SaveFilePath)));
                     }
                     else
                     {
@@ -58,13 +80,13 @@ namespace YuriGameJam2023.Persistency
 #if UNITY_EDITOR
             Debug.Log("Saving Data: " + _saveData);
 #endif
-            File.WriteAllText($"{Application.persistentDataPath}/save.bin", Encrypt(JsonConvert.SerializeObject(_saveData)));
+            File.WriteAllBytes(SaveFilePath, Encrypt(JsonConvert.SerializeObject(_saveData)));
         }
 
         public void DeleteSaveFolder()
         {
             _saveData = new();
-            File.Delete($"{Application.persistentDataPath}/save.bin");
+            File.Delete(SaveFilePath);
         }
     }
 }
