79abb17a51298894c24bfffc6f22c1d38924879c
Xwilarg <xwilarg@protonmail.com>
2023-11-19 12:14:08 +0100

    Implement easy mode

---
+++ Assets/Scripts/Map/DisableIfEasyMode.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using YuriGameJam2023.Persistency;
+
+namespace YuriGameJam2023.Map
+{
+    public class DisableIfEasyMode : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if (PersistencyManager.Instance.SaveData.IsEasyMode)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
