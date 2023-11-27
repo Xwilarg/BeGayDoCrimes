9cce79450eaa67c54c0eed7c2597fa9a454cb8c0
Hinara Turevel <hinara.turevel@gmail.com>
2023-11-27 21:24:38 +0100

    Fix Next button displayed for gallery with only one image

--- Assets/Scripts/Menu/Gallery.cs
+++ Assets/Scripts/Menu/Gallery.cs
@@ -13,6 +13,9 @@ namespace YuriGameJam2023.Menu
         [SerializeField]
         private Image _image;
 
+        [SerializeField]
+        private Button _next;
+
         private GalleryImageInfo[] _data;
 
         private int _index;
@@ -24,6 +27,7 @@ namespace YuriGameJam2023.Menu
 
             _text.text = _data[_index].Text;
             _image.sprite = _data[_index].Sprite;
+            _next.gameObject.SetActive(data.Length > 1);
         }
 
         public void Next()
